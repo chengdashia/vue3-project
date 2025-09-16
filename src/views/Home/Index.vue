969f6ab0558b9619deaa8b5a7ff4dd54009b2fdf
dby <17337995232@163.com>
2025-09-16 20:05:35 +0800

    home-面板组件封装

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -3,6 +3,7 @@ import HomeBanner from './HomeBanner.vue'
 import HomeCategory from './HomeCategory.vue'
 import HomeHot from './HomeHot.vue'
 import HomeNew from './HomeNew.vue'
+import HomePanel from './HomePanel.vue'
 import HomeProduct from './HomeProduct.vue'
 
 
@@ -16,4 +17,14 @@ import HomeProduct from './HomeProduct.vue'
     <HomeNew/>
     <HomeHot/>
     <HomeProduct/>
-</template>+
+    <!-- 测试 -->
+     <HomePanel title="测试标题" subTitle="测试副标题"> 
+        <div style="height: 200px; background: #f00">插槽内容</div>
+
+     </HomePanel>
+
+    <HomePanel title="测试标题2" subTitle="测试副标题2"> 
+        <div style="height: 200px; background: #0f0">插槽内容2</div>
+        
+     </HomePanel>
+</template>
